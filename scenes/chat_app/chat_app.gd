4f134e58a534771ca6cb917617745804ed4b2c1d
c-dez <hdezcruzcarlos@gmail.com>
2025-07-25 15:17:49 -0600

    agregado options_btns y dialogo relacionado

--- scenes/chat_app/chat_app.gd
+++ scenes/chat_app/chat_app.gd
@@ -7,6 +7,8 @@ extends Control
 
 var left_chat: Resource = preload("res://scenes/chat_app/chat_boxes/left/left_chat_box.tscn")
 var right_chat: Resource = preload("res://scenes/chat_app/chat_boxes/right/right_chat_box.tscn")
+#optionsBtns
+var options_btns: Resource = preload("res://scenes/chat_app/chat_boxes/options_btns/options_btns.tscn")
 
 var dialogo_actual = {}
 var index: int = 0
@@ -27,9 +29,30 @@ func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		imprimir_linea()
 		pass
-	
-	
+
+
 	pass
+
+func imprimir_opciones() -> void:
+	var instance: OptionsBtns = options_btns.instantiate()
+	v_box.add_child(instance)
+	instance.option_1.text = "texto1"
+	instance.option_2.text = "texto2"
+	#senales
+	instance.option_1.connect("pressed", Callable(self, "_on_option_1_pressed").bind(instance.option_1.text))
+	instance.option_2.connect("pressed", Callable(self, "_on_option_2_pressed").bind(instance.option_2.text))
+
+	await get_tree().process_frame
+	scroll_container.ensure_control_visible(instance)
+
+
+func _on_option_1_pressed(text):
+	print(text)
+
+
+func _on_option_2_pressed(text):
+	print(text)
+
 
 func imprimir_linea() -> void:
 	if dialogo_actual == null:
@@ -48,6 +71,21 @@ func imprimir_linea() -> void:
 			await get_tree().process_frame
 			scroll_container.ensure_control_visible(instance)
 			index += 1
+			pass
+
+
+		elif personaje == "opciones":
+			var instance: OptionsBtns = options_btns.instantiate()
+			v_box.add_child(instance)
+			instance.option_1.text = lineas[0]
+			instance.option_2.text = lineas[1]
+			#senales
+			instance.option_1.connect("pressed", Callable(self, "_on_option_1_pressed").bind(instance.option_1.text))
+			instance.option_2.connect("pressed", Callable(self, "_on_option_2_pressed").bind(instance.option_2.text))
+
+			await get_tree().process_frame
+			scroll_container.ensure_control_visible(instance)
+			index +=1
 			pass
 		else:
 			var instance: LeftChatBox = left_chat.instantiate()
---
+++ scenes/chat_app/chat_boxes/options_btns/options_btns.gd
@@ -0,0 +1,9 @@
+class_name OptionsBtns
+extends HBoxContainer
+
+
+@onready var option_1: Button = get_node("Button")
+@onready var option_2: Button = get_node("Button2")
+
+
+
