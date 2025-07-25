f73d58f7d733fa13409ac454dfa9c67db6492f46
c-dez <hdezcruzcarlos@gmail.com>
2025-07-25 15:44:31 -0600

    arreglado problemas de logica

--- scenes/chat_app/chat_app.gd
+++ scenes/chat_app/chat_app.gd
@@ -33,17 +33,17 @@ func _process(_delta: float) -> void:
 
 	pass
 
-func imprimir_opciones() -> void:
-	var instance: OptionsBtns = options_btns.instantiate()
-	v_box.add_child(instance)
-	instance.option_1.text = "texto1"
-	instance.option_2.text = "texto2"
-	#senales
-	instance.option_1.connect("pressed", Callable(self, "_on_option_1_pressed").bind(instance.option_1.text))
-	instance.option_2.connect("pressed", Callable(self, "_on_option_2_pressed").bind(instance.option_2.text))
+# func imprimir_opciones() -> void:
+# 	var instance: OptionsBtns = options_btns.instantiate()
+# 	v_box.add_child(instance)
+# 	instance.option_1.text = "texto1"
+# 	instance.option_2.text = "texto2"
+# 	#senales
+# 	instance.option_1.connect("pressed", Callable(self, "_on_option_1_pressed").bind(instance.option_1.text))
+# 	instance.option_2.connect("pressed", Callable(self, "_on_option_2_pressed").bind(instance.option_2.text))
 
-	await get_tree().process_frame
-	scroll_container.ensure_control_visible(instance)
+# 	await get_tree().process_frame
+# 	scroll_container.ensure_control_visible(instance)
 
 
 func _on_option_1_pressed(text):
@@ -56,7 +56,7 @@ func _on_option_2_pressed(text):
 
 func imprimir_linea() -> void:
 	if dialogo_actual == null:
-		print("dialogo null")
+		# print("dialogo null")
 		return
 
 	var lineas: Array = dialogo_actual["lineas"]
@@ -71,36 +71,36 @@ func imprimir_linea() -> void:
 			await get_tree().process_frame
 			scroll_container.ensure_control_visible(instance)
 			index += 1
-			pass
-
-
 		elif personaje == "opciones":
 			var instance: OptionsBtns = options_btns.instantiate()
 			v_box.add_child(instance)
-			instance.option_1.text = lineas[0]
-			instance.option_2.text = lineas[1]
+			# para evitar problemas lineas que es un array, contiene un array y este array contiene las dos opciones por eso las dos siguientes lineas
+			instance.option_1.text = lineas[0][0]
+			instance.option_2.text = lineas[0][1]
 			#senales
 			instance.option_1.connect("pressed", Callable(self, "_on_option_1_pressed").bind(instance.option_1.text))
 			instance.option_2.connect("pressed", Callable(self, "_on_option_2_pressed").bind(instance.option_2.text))
 
 			await get_tree().process_frame
 			scroll_container.ensure_control_visible(instance)
-			index +=1
-			pass
+			index += 1
 		else:
 			var instance: LeftChatBox = left_chat.instantiate()
 			v_box.add_child(instance)
 			instance.label.text = lineas[index]
+			
 			# asegurarse que el scroll se baje para que el nuevo nodo sea visible
 			await get_tree().process_frame
 			scroll_container.ensure_control_visible(instance)
 			index += 1
-			pass
 	else:
 		if dialogo_actual.has("siguiente"):
 			dialogo_actual = dialogo_actual["siguiente"]
 			index = 0
 			imprimir_linea()
+		else:
+			print("fin de dialogo")
+			dialogo_actual = null
 	
 
 func load_dialog(ruta: String):
