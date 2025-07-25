f33ce82585ec6a343d08337cca8a16a942db389a
c-dez <hdezcruzcarlos@gmail.com>
2025-07-25 01:08:10 -0600

    sistema basico terminado

--- scenes/interactive_menu/interactive_menu.gd
+++ scenes/interactive_menu/interactive_menu.gd
@@ -1,6 +1,6 @@
-class_name InteractiveMenu
+class_name InteractiveMenu # no me gusta este nombre cambiarlo por algo mas descriptivo
 extends Control
-
+# este nodo se encarga de crear las cajas de texto, su contenido y scroll
 
 @onready var scroll_container: ScrollContainer = get_node("ScrollContainer")
 @onready var v_box: VBoxContainer = scroll_container.get_node("VBoxContainer")
@@ -8,14 +8,64 @@ extends Control
 var left_chat: Resource = preload("res://scenes/interactive_menu/chat_boxes/left/left_chat_box.tscn")
 var right_chat: Resource = preload("res://scenes/interactive_menu/chat_boxes/right/right_chat_box.tscn")
 
+var dialogo_actual = {}
+var index: int = 0
+
+
+var text_content: String
+
+func _ready() -> void:
+	dialogo_actual = load_dialog("res://dialogos/dialogo.json") as Dictionary
+	print("presiona enter para avanzar")
+	
+	pass
+
 
 func _process(_delta: float) -> void:
-    if Input.is_action_just_pressed("ui_accept"):
-        var instance: Node = left_chat.instantiate()
-        v_box.add_child(instance)
-        # asegurarse que el scroll se baje para que el nuevo nodo sea visible
-        await get_tree().process_frame
-        scroll_container.ensure_control_visible(instance)
-    
-    
-    pass
+	# test
+	# crea un nodo y baja el scroll bar
+	if Input.is_action_just_pressed("ui_accept"):
+		imprimir_linea()
+		pass
+	
+	
+	pass
+
+func imprimir_linea() -> void:
+	if dialogo_actual == null:
+		return
+
+	var lineas: Array = dialogo_actual["lineas"]
+	var personaje: String = dialogo_actual["personaje"]
+
+	if index < lineas.size():
+		if personaje == "Caballero":
+			var instance: LeftChatBox = left_chat.instantiate()
+			v_box.add_child(instance)
+			instance.label.text = lineas[index]
+			# asegurarse que el scroll se baje para que el nuevo nodo sea visible
+			await get_tree().process_frame
+			scroll_container.ensure_control_visible(instance)
+			index += 1
+			pass
+		else:
+			var instance: RightChatBox = right_chat.instantiate()
+			v_box.add_child(instance)
+			instance.label.text = lineas[index]
+			# asegurarse que el scroll se baje para que el nuevo nodo sea visible
+			await get_tree().process_frame
+			scroll_container.ensure_control_visible(instance)
+			index += 1
+			pass
+	else:
+		if dialogo_actual.has("siguiente"):
+			dialogo_actual = dialogo_actual["siguiente"]
+			index = 0
+			imprimir_linea()
+	
+
+func load_dialog(ruta: String):
+	var archivo := FileAccess.open(ruta, FileAccess.READ)
+	var contenido: String = archivo.get_as_text()
+	var datos: Dictionary = JSON.parse_string(contenido)
+	return datos
