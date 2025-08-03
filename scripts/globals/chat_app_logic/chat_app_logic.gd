88a96f4b4850000548c42e4782752fdc31555358
c-dez <hdezcruzcarlos@gmail.com>
2025-08-02 18:50:13 -0600

    creado comportamiento basico al apretar una opcion el command de +1 int se ejecuta

--- scripts/globals/chat_app_logic/chat_app_logic.gd
+++ scripts/globals/chat_app_logic/chat_app_logic.gd
@@ -2,4 +2,15 @@ extends Node
 
 
 func _on_command_recived(command:String):
-	print("on command recived:", command)
+	command_decisions(command)
+
+
+func command_decisions(command:String):
+	match command:
+		"flarya int +1":
+			print("yeah")
+			pass
+		"flarya int +2":
+			print("2")
+		_:
+			pass
