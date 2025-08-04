9e8988644421d2dc47a8c4e3c978952b7fb9af2c
c-dez <hdezcruzcarlos@gmail.com>
2025-08-04 17:23:50 -0600

    creado DB para paths de retratos de furrys

--- scenes/chat_app/photo_frame/photo_frame.gd
+++ scenes/chat_app/photo_frame/photo_frame.gd
@@ -2,18 +2,11 @@ class_name PhotoFrame
 extends Control
 
 # view_port contiene como childs Sprite2D para almacenar las imagenes que se van a mostrar
-@onready var view_port: SubViewportContainer = get_node("SubViewportContainer")
+@onready var _view_port: SubViewportContainer = get_node("SubViewportContainer")
+@onready var sprite: Sprite2D = _view_port.get_node("Sprite2D")
 
+var sprite_texture_path: String
 
 func _ready() -> void:
-	check_if_no_images_in_view_port()
-
-
-func check_if_no_images_in_view_port() -> void:
-	if view_port.get_child_count() == 0:
-		print("no images in PhotoFrame")
-	else:
-		for i in range(view_port.get_child_count()):
-			var node: Node
-			node = view_port.get_child(i)
-			print(node.name)
+	if sprite.texture == null:
+		print("null")
---
+++ scripts/globals/DB/portraits_paths/portraits_paths.gd
@@ -0,0 +1,9 @@
+extends Node
+
+
+# global
+# contiene los paths de los retratos de furrys
+
+var flarya:Dictionary = {
+	"flarya":"res://nsfw/photos/furry1_test.jpg",
+}
