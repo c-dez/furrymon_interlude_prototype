e718b926ae765e01deb4f009a7f901131878657c
c-dez <hdezcruzcarlos@gmail.com>
2025-08-04 17:25:34 -0600

    comentado codigo

--- scripts/globals/DB/portraits_paths/portraits_paths.gd
+++ scripts/globals/DB/portraits_paths/portraits_paths.gd
@@ -3,7 +3,8 @@ extends Node
 
 # global
 # contiene los paths de los retratos de furrys
+# solo es prototipo nombres y estandares por confirmar
 
-var flarya:Dictionary = {
-	"flarya":"res://nsfw/photos/furry1_test.jpg",
+var flarya: Dictionary = {
+	"flarya_1": "res://nsfw/photos/furry1_test.jpg",
 }
