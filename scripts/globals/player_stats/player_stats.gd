049879ded6b041929791ac60e4fcde08a15bd947
c-dez <hdezcruzcarlos@gmail.com>
2025-08-19 22:43:53 -0600

    comentado _player_stats

--- scripts/globals/player_stats/player_stats.gd
+++ scripts/globals/player_stats/player_stats.gd
@@ -1,14 +1,19 @@
 extends Node
 # global
 
-
+## almacena la informacion y stats de jugador, es privado solo se accede directamente para save game, para lo demas se usan get/set
 var _player_stats: Dictionary = {
 	"player_name": "Default Name",
 	"player_id": "Player",
 	
 	"flarya": {
 		"interes": 0,
-	}
+		"algun_otro_stat": 0,
+	},
+	"algun_otro_furry": {
+		"interes": 0,
+		"example_stat": 0,
+	},
 }
 
 func set_atribute(value: int, furry_name: String, atribute_name: String) -> void:
@@ -17,6 +22,7 @@ func set_atribute(value: int, furry_name: String, atribute_name: String) -> void
 
 func get_atribute(furry_name: String, atribute_name: String) -> int:
 	return _player_stats[furry_name][atribute_name]
+
 
 func get_player_id() -> String:
 	return _player_stats["player_id"]
